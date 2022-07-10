9b600f62d11b1c8257d8385b666eb9df09f7b6ca
DESKTOP-0STHDG7\Administrator <fengzyworks@163.com>
2022-07-11 00:57:26 +0800

    add cli files

--- bin/index.js
+++ bin/index.js
@@ -1,3 +1,29 @@
 #! /usr/bin/env node
 
-console.log('fz-cli 脚手架工具');
+// cli info
+const {
+  name,
+  version,
+  description
+} = require('../package.json')
+// console.log(`${name} ${version}\n${description}`);
+
+// commander 
+const {
+  Command
+} = require('commander');
+const program = new Command();
+
+// cli help info
+program.name(name).description(description).version(version, '-v, -V, --version', `${name} 版本号`)
+
+// cli commands
+// [mode] 可选参数
+// <name> 必选参数
+program.command('create <name> [mode]').description('脚手架创建项目模板').action((model, name) => {
+  console.log('model',model, name);
+  require('../lib/create')(name)
+})
+
+
+program.parse()
